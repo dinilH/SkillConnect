348d3458785e8c127b6529d16fe11765c2842d6d
K.A.I.Dilum <isiradilumuni@gmail.com>
2025-12-02 19:27:53 +0530

    Remove .next folder from repo

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oLV2JR3UsXeHf/5KpZ6pSvLsKEF173ATSWaiDIJkcJw=\"\n}"
--- .next/dev/static/chunks/src_app_page_tsx_027fdfa5._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_65863696._.js"
-],
-    source: "dynamic"
-});
